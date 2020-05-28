38dbc033734de20718dac8c7b17e404d3751af32
eyadal <31968847+eyadal@users.noreply.github.com>
2020-05-28 16:06:37 +0200

    how to use a forEach loop

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,12 @@ const companies = [
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // foor loop //
 
-for (let i = 0; i < companies.length; i++) {
-    console.log(companies[i])
-}
+// for (let i = 0; i < companies.length; i++) {
+//     console.log(companies[i])
+// }
 
+// forEach //
+companies.forEach(function(company) {
+    console.log(company.name)
+    })
+    
