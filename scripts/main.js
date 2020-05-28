3a5eef727043b987820e607103117b52fb7feb3c
eyadal <31968847+eyadal@users.noreply.github.com>
2020-05-28 17:05:50 +0200

    ES 5 filter methody

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //     console.log(company.name)
 //     })
 
-
+// Get 21 or older
 // let canDrink = [];
 // for (let i = 0; i < ages.length; i++) {
 //     if (ages[i] >= 21) {
@@ -37,5 +37,12 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // })
 //  filter method with Arrow function
 const canDrink = ages.filter(age => age >= 21)
-console.log(canDrink)
+
+// filter retail companies
+const retailCompanies = companies.filter(function (company) {
+    if (company.category === 'Retail') {
+        return true
+    }
+})
+console.log(retailCompanies)
 
