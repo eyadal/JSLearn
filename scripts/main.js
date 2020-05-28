cfad6a78d9b5aadea2b284c523387680691bc875
eyadal <31968847+eyadal@users.noreply.github.com>
2020-05-28 16:07:49 +0200

    how to use filter method in a for loop

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,14 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // }
 
 // forEach //
-companies.forEach(function(company) {
-    console.log(company.name)
-    })
+// companies.forEach(function(company) {
+//     console.log(company.name)
+//     })
     
+// filter //
+let canDrink = [];
+for (let i = 0; i < ages.length; i++) {
+    if(ages[i] >=21) {
+        canDrink.push(ages[i])
+    }
+}
