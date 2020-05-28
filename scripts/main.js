9f01430cb22f6607b9ab1a9d649b23734c9e5d34
eyadal <31968847+eyadal@users.noreply.github.com>
2020-05-28 16:59:24 +0200

    filter method with Arrow function

--- scripts/main.js
+++ scripts/main.js
@@ -30,10 +30,12 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //     }
 // }
 // filter //
-const canDrink = ages.filter(function (age) {
-    if (age >= 21) {
-        return true;
-    }
-})
+// const canDrink = ages.filter(function (age) {
+//     if (age >= 21) {
+//         return true;
+//     }
+// })
+//  filter method with Arrow function
+const canDrink = ages.filter(age => age >= 21)
 console.log(canDrink)
 
